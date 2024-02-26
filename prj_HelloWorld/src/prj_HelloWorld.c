f1a1bc9b693fb328caaf1c0c6823d4bcf74d17f2
Pedro Henrique Ferraz Lima <pedrohferraz21@gmail.com>
2024-02-26 12:07:57 -0300

    Add files via upload

---
+++ prj_HelloWorld/src/prj_HelloWorld.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : prj_HelloWorld.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14159
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+
+	int idade = 26;
+	const int MAX_NUM = 100;
+
+	printf("Minha variável int comum = %d\n", idade);
+	printf("Minha variável int constante = %d\n", MAX_NUM);
+	printf("Minha constante = %f\n", PI);
+
+	//MAX_NUM = 99;
+
+	return EXIT_SUCCESS;
+}
+
